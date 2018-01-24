a1f57f7dc8010f4d55be23a145bd5a33c493838b
Nazar <nazar@sakharenko.com>
2018-01-24 15:48:24 +0200

    feat: .editorconfig; Use encryption by default

--- src/crypto.ts
+++ src/crypto.ts
@@ -1,31 +1,38 @@
-import {Cipher, createCipher, createDecipher, Decipher, HexBase64BinaryEncoding, Utf8AsciiBinaryEncoding} from 'crypto';
+import {
+  Cipher,
+  createCipher,
+  createDecipher,
+  Decipher,
+  HexBase64BinaryEncoding,
+  Utf8AsciiBinaryEncoding,
+} from 'crypto';
 
 const PASSWORD = 'adsfds221213askjldfk!-dsd@sdafas asd asjsda';
 
 export default class Crypto {
-    private static ALGORITHM = 'aes-256-ctr';
-    private static INPUT_ENCODING: Utf8AsciiBinaryEncoding = 'utf8';
-    private static OUTPUT_ENCODING: HexBase64BinaryEncoding = 'hex';
+  private static ALGORITHM = 'aes-256-ctr';
+  private static INPUT_ENCODING: Utf8AsciiBinaryEncoding = 'utf8';
+  private static OUTPUT_ENCODING: HexBase64BinaryEncoding = 'hex';
 
-    private password: string;
-    private cipher: Cipher;
-    private decipher: Decipher;
+  private password: string;
+  private cipher: Cipher;
+  private decipher: Decipher;
 
-    constructor(password: string = PASSWORD) {
-        this.password = password;
-        this.cipher = createCipher(Crypto.ALGORITHM, this.password);
-        this.decipher = createDecipher(Crypto.ALGORITHM, this.password);
-    }
+  constructor(password: string = PASSWORD) {
+    this.password = password;
+    this.cipher = createCipher(Crypto.ALGORITHM, this.password);
+    this.decipher = createDecipher(Crypto.ALGORITHM, this.password);
+  }
 
-    encrypt(plainText: string): string {
-        return this
-            .cipher
-            .update(plainText, Crypto.INPUT_ENCODING, Crypto.OUTPUT_ENCODING) + this.cipher.final(Crypto.OUTPUT_ENCODING);
-    }
+  encrypt(plainText: string): string {
+    return this
+      .cipher
+      .update(plainText, Crypto.INPUT_ENCODING, Crypto.OUTPUT_ENCODING) + this.cipher.final(Crypto.OUTPUT_ENCODING);
+  }
 
-    decrypt(encryptedText: string): string {
-        return this
-            .decipher
-            .update(encryptedText, Crypto.OUTPUT_ENCODING, Crypto.INPUT_ENCODING) + this.decipher.final(Crypto.INPUT_ENCODING);
-    }
+  decrypt(encryptedText: string): string {
+    return this
+      .decipher
+      .update(encryptedText, Crypto.OUTPUT_ENCODING, Crypto.INPUT_ENCODING) + this.decipher.final(Crypto.INPUT_ENCODING);
+  }
 }
--- src/store.ts
+++ src/store.ts
@@ -5,43 +5,46 @@ import * as path from 'path';
 import Crypto from './crypto';
 
 export class Store {
-    private data: any;
-    private path: any;
-    private crypt: Crypto = new Crypto();
+  private data: any;
+  private path: any;
+  private encrypt: boolean;
+  private crypt: Crypto = new Crypto();
 
-    constructor(opts) {
-        // Renderer process has to get `app` module via `remote`, whereas the main process can get it directly
-        // app.getPath('userData') will return a string of the user's app data directory path.
-        const userDataPath = (electron.app || electron.remote.app).getPath('userData');
-        // We'll use the `configName` property to set the file name and path.join to bring it all together as a string
-        this.path = path.join(userDataPath, opts.configName + '.json');
+  constructor(opts) {
+    // Renderer process has to get `app` module via `remote`, whereas the main process can get it directly
+    // app.getPath('userData') will return a string of the user's app data directory path.
+    const userDataPath = (electron.app || electron.remote.app).getPath('userData');
+    // We'll use the `configName` property to set the file name and path.join to bring it all together as a string
+    this.path = path.join(userDataPath, opts.configName + '.json');
+    this.encrypt = (opts.encrypt === true);
+    this.data = this.parseDataFile(this.path, opts.defaults);
+  }
 
-        this.data = parseDataFile(this.path, opts.defaults);
-    }
+  // This will just return the property on the `data` object
+  get(key: string) {
+    return this.data[key];
+  }
 
-    // This will just return the property on the `data` object
-    get(key: string, decrypt?: boolean) {
-        return decrypt ? this.crypt.decrypt(this.data[key]) : this.data[key];
-    }
+  // ...and this will set it
+  set(key: string, val: any) {
+    this.data[key] = val;
+    // Wait, I thought using the node.js' synchronous APIs was bad form?
+    // We're not writing a server so there's not nearly the same IO demand on the process
+    // Also if we used an async API and our app was quit before the asynchronous write had a chance to complete,
+    // we might lose that data. Note that in a real app, we would try/catch this.
+    const content = JSON.stringify(this.data);
+    fs.writeFileSync(this.path, this.encrypt ? this.crypt.encrypt(content) : content);
+  }
 
-    // ...and this will set it
-    set(key: string, val: any, encrypt?: boolean) {
-        this.data[key] = encrypt ? this.crypt.encrypt(val) : val;
-        // Wait, I thought using the node.js' synchronous APIs was bad form?
-        // We're not writing a server so there's not nearly the same IO demand on the process
-        // Also if we used an async API and our app was quit before the asynchronous write had a chance to complete,
-        // we might lose that data. Note that in a real app, we would try/catch this.
-        fs.writeFileSync(this.path, JSON.stringify(this.data));
-    }
-}
-
-function parseDataFile(filePath, defaults) {
+  private parseDataFile(filePath, defaults) {
     // We'll try/catch it in case the file doesn't exist yet, which will be the case on the first application run.
     // `fs.readFileSync` will return a JSON string which we then parse into a Javascript object
     try {
-        return JSON.parse(fs.readFileSync(filePath).toString());
+      const data = fs.readFileSync(filePath).toString();
+      return JSON.parse(this.encrypt ? this.crypt.decrypt(data) : data);
     } catch (error) {
-        // if there was some kind of error, return the passed in defaults instead.
-        return defaults;
+      // if there was some kind of error, return the passed in defaults instead.
+      return defaults;
     }
+  }
 }
